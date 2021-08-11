ac24cf0df6950b86dc64d2c904994e7e2ec880b7
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-08-11 13:35:11 +0400

    Feat: Added increment and decrement

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,9 +1,23 @@
+import { useSelector, useDispatch } from "react-redux";
+import {increment, decrement} from "../../libs/store/features/counter";
+
 export default function Counter() {
+    const dispatch = useDispatch();
+    const count = useSelector((state) => state.counter.value);
+
+    const handleInc = () => {
+        dispatch(increment());
+    };
+
+    const handleDec = () => {
+        dispatch(decrement())
+    }
+
     return (
         <div>
-            0
-            <button>+</button>
-            <button>-</button>
+            {count}
+            <button onClick={handleInc}>+</button>
+            <button onClick={handleDec}>-</button>
         </div>
     );
 }
